568df2b25259889a1cded6b135b719b48b4ae1af
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-09-10 13:06:04 +0200

    naming

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ sudo docker ps -a | tail -n +2 | awk '{print $1}' | xargs sudo docker rm
 echo "---"
 echo "setting up master"
 echo "---"
-export MASTER_ID=$(sudo docker run -p 29015:29015 -p 28015:28015 -d mies/rethink rethinkdb --bind all)
+export MASTER_ID=$(sudo docker run -p 29015:29015 -p 28015:28015 -d mies/rethinkdb rethinkdb --bind all)
 export MASTER_IP=$(sudo docker inspect $MASTER_ID | grep IPAddress | cut -d '"' -f 4)
 echo $MASTER_ID
 export MASTER_CLUSTER_PORT=$(sudo docker port $MASTER_ID 29015)
@@ -15,7 +15,7 @@ echo $MASTER_PORT
 echo "---"
 echo "setting up first slave"
 echo "---"
-export SLAVE1_ID=$(sudo docker run -p 29015 -p 28015 -d mies/rethink rethinkdb --join $MASTER_IP:$MASTER_CLUSTER_PORT --bind all)
+export SLAVE1_ID=$(sudo docker run -p 29015 -p 28015 -d mies/rethinkdb rethinkdb --join $MASTER_IP:$MASTER_CLUSTER_PORT --bind all)
 echo $SLAVE1_ID
 export SLAVE1_PORT=$(sudo docker port $SLAVE1_ID 28015)
 echo $SLAVE1_PORT
@@ -25,7 +25,7 @@ echo $SLAVE1_IP
 echo "---"
 echo "setting up second slave"
 echo "---"
-export SLAVE2_ID=$(sudo docker run -p 49235:29015 -p 49236:28015 -d mies/rethink rethinkdb --join $MASTER_IP:$MASTER_CLUSTER_PORT --bind all)
+export SLAVE2_ID=$(sudo docker run -p 49235:29015 -p 49236:28015 -d mies/rethinkdb rethinkdb --join $MASTER_IP:$MASTER_CLUSTER_PORT --bind all)
 echo $SLAVE2_ID
 export SLAVE2_PORT=$(sudo docker port $SLAVE2_ID 28015)
 export SLAVE2_IP=$(sudo docker inspect $SLAVE2_ID | grep IPAddress | cut -d '"' -f 4)
